ea9630b1fb3cd17b18cfa4ec61d59132db970176
ananthchavancs24-prog <ananthchavan.cs24@bmsce.ac.in>
2026-04-13 09:50:34 +0530

    Add files via upload

---
+++ lab-5.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define MAX 10
+
+int main() {
+    int n, i, j;
+    int adj[MAX][MAX], indegree[MAX] = {0};
+    int queue[MAX], front = 0, rear = -1;
+    int topo[MAX], k = 0;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(adj[i][j] == 1) {
+                indegree[j]++;
+            }
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+        if(indegree[i] == 0) {
+            queue[++rear] = i;
+        }
+    }
+
+    while(front <= rear) {
+        int v = queue[front++];
+        topo[k++] = v;
+
+        for(j = 0; j < n; j++) {
+            if(adj[v][j] == 1) {
+                indegree[j]--;
+                if(indegree[j] == 0) {
+                    queue[++rear] = j;
+                }
+            }
+        }
+    }
+
+    if(k != n) {
+        printf("Graph has a cycle! No Topological Sort possible.\n");
+    } else {
+        printf("Topological Order:\n");
+        for(i = 0; i < n; i++) {
+            printf("%d ", topo[i]);
+        }
+    }
+
+    return 0;
+}
